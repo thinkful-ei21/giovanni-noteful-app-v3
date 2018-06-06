05fcf0af138019e5b5da48b4f393333024847f2e
giovanni <mormagli@gmail.com>
2018-06-06 18:46:41 -0500

    provisioned mlab test-db for travis testing

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 
 exports.PORT = process.env.PORT || 8080;
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://dev:098poi@ds255889.mlab.com:55889/noteful-v3';
-exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://dev:098poi@ds251210.mlab.com:51210/test-db';
